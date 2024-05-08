b7e44e5a692becf3b623e6fbdd1695614d34ce72
douglas lima <douglaslima13bezerra@gmail.com>
2024-05-08 11:37:15 -0300

    feat(backend): add remove and get foods

--- backend/src/controllers/foodController.ts
+++ backend/src/controllers/foodController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { foodModel } from '../models/foodModel';
 import { IFood } from '../types/food';
+import fs from 'fs';
 
 // add food item
 export const addFoodController = async (
@@ -28,12 +29,59 @@ export const addFoodController = async (
 
         await food.save();
 
+        // MUNDAR: ao inver de colocar uma response retornar o tipo de response e no food routes pe responsavel de mostrar
         res.status(201).json({
             success: true,
             message: 'Food added successfully',
         });
     } catch (error) {
         console.error('Error adding food:', error);
+        res.status(500).json({
+            success: false,
+            message: 'Internal server error',
+        });
+    }
+};
+
+// get food
+export const getFoodController = async (req: Request, res: Response) => {
+    try {
+        const foods: IFood[] = await foodModel.find({});
+
+        if (!foods) {
+            res.status(404).json({
+                success: false,
+                message: 'No foods found',
+            });
+            return;
+        }
+
+        res.status(200).json({
+            success: true,
+            data: foods,
+        });
+    } catch (error) {
+        console.error('Error getting all foods: ', error);
+        res.status(500).json({
+            success: false,
+            message: 'Internal server error',
+        });
+    }
+};
+
+export const removeFoodController = async (req: Request, res: Response) => {
+    try {
+        const food = await foodModel.findById(req.body.id);
+
+        fs.unlink(`uploads/${food.image}`, () => {});
+
+        await foodModel.findByIdAndDelete(req.body.id);
+
+        res.status(200).json({
+            success: true,
+            message: 'Food Removed',
+        });
+    } catch (error) {
         res.status(500).json({
             success: false,
             message: 'Internal server error',
--- backend/src/routes/foodRoute.ts
+++ backend/src/routes/foodRoute.ts
@@ -1,5 +1,9 @@
 import { Router, Request, Response } from 'express';
-import { addFoodController } from '../controllers/foodController';
+import {
+    addFoodController,
+    getFoodController,
+    removeFoodController,
+} from '../controllers/foodController';
 import { upload } from '../config/multer';
 
 export const foodRouter: Router = Router();
@@ -10,6 +14,14 @@ foodRouter.post(
     (req: Request, res: Response) => {
         addFoodController(req, res);
 
-        res.send('Food added successfully');
+        // res.send('Food added successfully');
     }
 );
+
+foodRouter.get('/get', (req: Request, res: Response) => {
+    getFoodController(req, res);
+});
+
+foodRouter.delete('/delete', (req: Request, res: Response) => {
+    removeFoodController(req, res);
+});
